289c5ff49609dfcf30aac96f9c807c4b7a2a68cd
Abdelrahman Sherif <dodger@Abdelrahmans-MacBook-Pro.local>
2025-10-04 06:49:07 +0300

    Chore:Admin-Layout

--- law-firm/src/layouts/AdminLayout.jsx
+++ law-firm/src/layouts/AdminLayout.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom"
+import SideBarContainer from './SideBar/SideBarContainer'
 
 function AdminLayout() {
     return (
         <>
+            <SideBarContainer />
+            <Outlet />
         </>
     )
 }
