b2f8096bbcc90961f869416b2c7e70b6a64e5afd
Abdelrahman Sherif <dodger@Abdelrahmans-MacBook-Pro.local>
2025-10-06 20:43:44 +0300

    feat: Add Language Support

--- law-firm/src/layouts/AdminLayout.jsx
+++ law-firm/src/layouts/AdminLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import SideBarContainer from "../components/AdminComponents/SideBar/SideBarContainer";
+import HeaderComponentContainer from "../components/AdminComponents/Header/HeaderComponentContainer";
 
 export default function AdminLayout() {
     return (
-        <div className="flex min-h-screen bg-gray-100">
+        <div className="flex">
             <SideBarContainer />
-            <main className="flex-1 p-6">
-                <Outlet />
-            </main>
+            <div className="flex flex-col w-full min-h-screen bg-gray-100">
+                <HeaderComponentContainer />
+                <main className="flex-1 p-6">
+                    <Outlet />
+                </main>
+            </div>
         </div>
     );
 }
